59c6bfc34604d0b56acd067c6440ab6122e537db
Essekia Paul <essekiarockz@gmail.com>
2024-05-16 11:34:20 +0530

    train_bible.py

--- config/train_bible.py
+++ config/train_bible.py
@@ -2,8 +2,8 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-bible'
-eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 40
+eval_interval = 2500 # keep frequent because we'll overfit
+eval_iters = 400
 log_interval = 5 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -15,19 +15,19 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'bible'
 gradient_accumulation_steps = 1
-batch_size = 24
-block_size = 128 # context of up to 256 previous characters
+batch_size = 240
+block_size = 1280 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 150
+n_layer = 60
+n_head = 60
+n_embd = 1500
 dropout = 0.1
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
+learning_rate = 1e-4 # with baby networks can afford to go a bit higher
+max_iters = 50000
+lr_decay_iters = 50000 # make equal to max_iters usually
+min_lr = 1e-5 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
