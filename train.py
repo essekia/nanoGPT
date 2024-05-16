d833ed8818914fe83818e0f440cb83e514997376
Essekia Paul <essekiarockz@gmail.com>
2024-05-16 11:32:11 +0530

    deploy v1

--- train.py
+++ train.py
@@ -33,9 +33,9 @@ from model import GPTConfig, GPT
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
-eval_interval = 2000
+eval_interval = 10000
 log_interval = 1
-eval_iters = 200
+eval_iters = 1000
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
@@ -46,12 +46,12 @@ wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
 gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
-batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
-block_size = 1024
+batch_size = 48 # if gradient_accumulation_steps > 1, this is the micro-batch size
+block_size = 4096
 # model
-n_layer = 12
-n_head = 12
-n_embd = 768
+n_layer = 48
+n_head = 48
+n_embd = 3072
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
