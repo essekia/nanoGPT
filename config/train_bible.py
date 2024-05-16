1dd7c66e726d3d1a45304656c0e01aebc58f5dce
Essekia Paul <essekiarockz@gmail.com>
2024-05-16 17:37:36 +0530

    latest working changes from 4090 - 24gb

--- config/train_bible.py
+++ config/train_bible.py
@@ -15,19 +15,19 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'bible'
 gradient_accumulation_steps = 1
-batch_size = 240
-block_size = 1280 # context of up to 256 previous characters
+batch_size = 24
+block_size = 128 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 60
-n_head = 60
-n_embd = 1500
+n_layer = 50
+n_head = 50
+n_embd = 1000
 dropout = 0.1
 
 learning_rate = 1e-4 # with baby networks can afford to go a bit higher
-max_iters = 50000
-lr_decay_iters = 50000 # make equal to max_iters usually
-min_lr = 1e-5 # learning_rate / 10 usually
+max_iters = 25000
+lr_decay_iters = 5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
